e7ab1f3219c6a5abb2d406d18581f6a6f905f372
seongh7800 <seongh7800@gmail.com>
2018-08-08 21:00:35 +0900

    검색폼 구현

--- src/components/CommonTest/Test.vue
+++ src/components/CommonTest/Test.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <h1>공통 컴포넌트 테스트</h1>
+
+    <v-flex xs12 sm3>
+      <search-form />
+    </v-flex>
+
   </div>
 </template>
+
+
+
 <script>
+import { SearchForm } from '../commons/Form'
+
   export default {
-    name: 'Test'
+    name: 'Test',
+
+
+    components: {
+      SearchForm
+    },
   }
   
 </script>
---
+++ src/components/commons/Form/SearchForm.vue
@@ -0,0 +1,71 @@
+<template>
+<v-form v-model="valid" style="position:relative;">
+    <v-text-field
+        v-model="name"
+        :rules="nameRules"
+        :counter="10"
+        label="Name"
+        required
+    />
+    <v-btn icon style="position:absolute; top:-8px; right:-10px;">
+        <v-icon>search</v-icon>
+    </v-btn>
+</v-form>
+</template>
+
+
+
+
+<script>
+export default{
+    name: 'SearchForm',
+
+    // ========== props ========== //
+    props:{
+        'label' : {
+            type: String
+        },
+
+        'options': {
+            type: Boolean
+        }
+
+    }, // ========== props ========== //
+
+
+
+    // ========== data ========== //
+    data(){
+        return {
+
+
+
+        }
+    }, // ========== data ========== //
+    
+    
+    
+    
+    // ========== created ========== //
+    created(){
+    }, // ========== created ========== //
+    
+    
+    
+
+
+    // ========== updated ========== //
+    updated(){
+    }, // ========== updated ========== //
+    
+    
+
+
+
+    
+    // ========== data ========== //
+    methods:{
+        
+    }, // ========== data ========== //
+}
+</script>
